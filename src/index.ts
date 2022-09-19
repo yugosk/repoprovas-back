7ccc0845dde6004b9bfb8f2266412bb76fdb65dc
Yugo <yugosakamoto0717@gmail.com>
2022-09-19 15:53:23 -0300

    [chore] adjusts to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(4000, () => console.log("Server running on PORT 4000"));
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
